dd822af57ef72363d21ed307b7652cf4235e3bc7
Matteo Lulli <matteo.lulli@gmail.com>
2022-04-15 22:27:57 +0800

    fixed CUDA_T management

--- idpy/CUDA/__init__.py
+++ idpy/CUDA/__init__.py
@@ -37,3 +37,5 @@ _idea_dot_deploy_path = os.path.dirname(os.path.abspath(_module_abs_path + "../.
 append to sys path in order to avoid relative imports
 '''
 sys.path.append(_idea_dot_deploy_path)
+
+CUDA_T = "pycuda"
