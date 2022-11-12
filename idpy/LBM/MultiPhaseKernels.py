9968a1f63f70135b948f10b0e7763859d56b1945
lullimat <matteo.lulli@gmail.com>
2022-11-12 22:43:22 +0800

    added collision with walls and psi with walls

--- idpy/LBM/MultiPhaseKernels.py
+++ idpy/LBM/MultiPhaseKernels.py
@@ -203,6 +203,91 @@ class K_Collision_ShanChenGuoMultiPhase(IdpyKernel):
         }
         """
 
+class K_Collision_ShanChenGuoMultiPhaseWalls(IdpyKernel):
+    def __init__(self, custom_types = {}, constants = {}, f_classes = [],
+                 optimizer_flag = None):
+        IdpyKernel.__init__(self, custom_types = custom_types,
+                            constants = constants, f_classes = f_classes,
+                            optimizer_flag = optimizer_flag)
+        self.SetCodeFlags('g_tid')
+        self.params = {'PopType * pop': ['global', 'restrict'],
+                       'UType * u': ['global', 'restrict'],
+                       'NType * n': ['global', 'restrict', 'const'],
+                       'PsiType * psi': ['global', 'restrict', 'const'],
+                       'SType * XI_list': ['global', 'restrict', 'const'],
+                       'WType * W_list': ['global', 'restrict', 'const'],
+                       'SType * E_list': ['global', 'restrict', 'const'],
+                       'WType * EW_list': ['global', 'restrict', 'const'],
+                       'SType * dim_sizes': ['global', 'restrict', 'const'],
+                       'SType * dim_strides': ['global', 'restrict', 'const'], 
+                       'FlagType * walls': ['global', 'restrict', 'const']}
+        
+        self.kernels[IDPY_T] = """
+        if(g_tid < V && walls[g_tid]){
+            // Getting thread position
+            SType g_tid_pos[DIM];
+            F_PosFromIndex(g_tid_pos, dim_sizes, dim_strides, g_tid);
+
+            // Computing Shan-Chen Force
+            SCFType F[DIM]; SType neigh_pos[DIM];
+            for(int d=0; d<DIM; d++){F[d] = 0.;}
+
+            PsiType lpsi = psi[g_tid];
+
+            for(int qe=0; qe<QE; qe++){
+                // Compute neighbor position
+                for(int d=0; d<DIM; d++){
+                    neigh_pos[d] = ((g_tid_pos[d] + E_list[d + qe*DIM] + dim_sizes[d]) % dim_sizes[d]);
+                }
+                // Compute neighbor index
+                SType neigh_index = F_IndexFromPos(neigh_pos, dim_strides);
+                // Get the pseudopotential value
+                PsiType npsi = psi[neigh_index];
+                // Add partial contribution
+                for(int d=0; d<DIM; d++){F[d] += E_list[d + qe*DIM] * EW_list[qe] * npsi;}
+            }
+            for(int d=0; d<DIM; d++){F[d] *= -SC_G * lpsi;}
+
+            // Local density and velocity for Guo velocity shift and equilibrium
+            NType ln = n[g_tid]; UType lu[DIM];
+
+            // Guo velocity shift & Copy to global memory
+            for(int d=0; d<DIM; d++){ 
+                lu[d] = u[g_tid + V*d] + 0.5 * F[d]/ln;
+                u[g_tid + V*d] = lu[d];
+            }
+
+            // Compute square norm of Guo shifted velocity
+            UType u_dot_u = 0., F_dot_u = 0.;
+            for(int d=0; d<DIM; d++){u_dot_u += lu[d]*lu[d]; F_dot_u  += F[d] * lu[d];}
+
+            // Cycle over the populations: equilibrium + Guo
+            for(int q=0; q<Q; q++){
+                UType u_dot_xi = 0., F_dot_xi = 0.; 
+                for(int d=0; d<DIM; d++){
+                    u_dot_xi += lu[d] * XI_list[d + q*DIM];
+                    F_dot_xi += F[d] * XI_list[d + q*DIM];
+                }
+
+                PopType leq_pop = 1., lguo_pop = 0.;
+
+                // Equilibrium population
+                leq_pop += + u_dot_xi*CM2 + 0.5*u_dot_xi*u_dot_xi*CM4;
+                leq_pop += - 0.5*u_dot_u*CM2;
+                leq_pop = leq_pop * ln * W_list[q];
+
+                // Guo population
+                lguo_pop += + F_dot_xi*CM2 + F_dot_xi*u_dot_xi*CM4;
+                lguo_pop += - F_dot_u*CM2;
+                lguo_pop = lguo_pop * W_list[q];
+
+                pop[g_tid + q*V] = \
+                    pop[g_tid + q*V]*(1. - OMEGA) + leq_pop*OMEGA + (1. - 0.5 * OMEGA) * lguo_pop;
+
+             }
+        }
+        """        
+
 class K_ComputePsi(IdpyKernel):
     def __init__(self, custom_types = None, constants = {}, f_classes = [], psi_code = None,
                  optimizer_flag = None):
@@ -222,6 +307,29 @@ class K_ComputePsi(IdpyKernel):
         if(g_tid < V){
             NType ln = n[g_tid];
             psi[g_tid] = """ + self.psi_code + """; }"""
+
+
+class K_ComputePsiWalls(IdpyKernel):
+    def __init__(self, custom_types = None, constants = {}, f_classes = [], psi_code = None,
+                 optimizer_flag = None):
+        if psi_code is None:
+            raise Exception("Missing argument psi_code")
+
+        self.psi_code = psi_code
+        
+        IdpyKernel.__init__(self, custom_types = custom_types,
+                            constants = constants, f_classes = f_classes,
+                            optimizer_flag = optimizer_flag)
+        self.SetCodeFlags('g_tid')
+        self.params = {'PsiType * psi': ['global', 'restrict'],
+                       'NType * n': ['global', 'restrict', 'const'], 
+                       'FlagType * walls': ['global', 'restrict', 'const']}
+
+        self.kernels[IDPY_T] = """
+        if(g_tid < V && walls[g_tid]){
+            NType ln = n[g_tid];
+            psi[g_tid] = """ + self.psi_code + """; }"""
+
 
 class K_InitFlatInterface(IdpyKernel):
     '''
