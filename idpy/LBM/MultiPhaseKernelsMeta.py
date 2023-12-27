385be251cd139303e1bfd843b546e8503ffc0481
lullimat <matteo.lulli@gmail.com>
2023-12-27 12:46:57 +0800

    added kernel 'K_ForceCollideSCMPMeta' to compute the collision with Guo+Shan-Chen without streaming

--- idpy/LBM/MultiPhaseKernelsMeta.py
+++ idpy/LBM/MultiPhaseKernelsMeta.py
@@ -344,6 +344,183 @@ class K_ForceCollideStreamSCMPMeta(IdpyKernel):
         }
         """
 
+class K_ForceCollideSCMPMeta(IdpyKernel):
+    def __init__(self, custom_types = {}, constants = {}, f_classes = [],
+                 optimizer_flag = None, 
+                 XIStencil = None, SCFStencil = None, use_ptrs = False, 
+                 ordering_lambda_pop = None, ordering_lambda_u = None,
+                 collect_mul = False, stream_mode = 'push', pressure_mode = 'compute',
+                 root_dim_sizes = 'L', root_strides = 'STR', root_coord = 'x'):
+        
+        self.expect_lambda_args = 2
+
+        if ordering_lambda_pop is None:
+            raise Exception("Missing argument 'ordering_lambda_pop'")
+        elif ordering_lambda_pop.__code__.co_argcount != self.expect_lambda_args:
+            raise Excpetion(
+                "The number of arguments for 'ordering_lambda_pop' should be ",
+                self.expect_lambda_args
+            )
+
+        if XIStencil is None:
+            raise Exception("Missing argument 'XIStencil'")
+        if SCFStencil is None:
+            raise Exception("Missing argument 'SCFStencil'")
+
+        self.idpy_stencil = IdpyStencil(XIStencil)
+        self.idpy_sc_force = IDShanChenForce(SCFStencil)
+        self.idpy_guo_forcing = GuoForcing(XIStencil, root_u = 'lu_0', root_f = 'd_psi', 
+                                           root_omega = '\\omega')
+        self.idpy_equilibria = HermiteEquilibria(XIStencil, root_pop = 'lpop', 
+                                                 root_n = 'ln_0', root_u = 'lu_0')
+        self.idpy_collision = \
+            BGK(root_omega = '\\omega', omega_val = constants['OMEGA'])
+        
+        IdpyKernel.__init__(self, custom_types = custom_types, constants = constants,
+                            f_classes = f_classes, optimizer_flag = optimizer_flag)
+
+        '''
+        Kernel Parameters declaration
+        '''
+        self.SetCodeFlags('g_tid')
+        self.params = {'PopType * pop': ['global', 'restrict'],
+                       'NType * n': ['global', 'restrict', 'const'],
+                       'PsiType * psi': ['global', 'restrict', 'const']}
+
+        '''
+        Setting the declared variables and constants up to this point
+        '''
+        self.SetDeclaredVariables()
+        self.SetDeclaredConstants()
+        self.dim_sizes_macros = _get_seq_macros(self.constants['DIM'], root_dim_sizes)
+        self.dim_strides_macros = \
+            _get_seq_macros(
+                self.constants['DIM'] - 1 if self.constants['DIM'] > 1 else 1, 
+                root_strides)
+
+        '''
+        Kernel Body
+        '''
+        # 1. Getting Cartesian coordinates
+        # 2. Compute the Shan-Chen force
+        # 3. Getting the local populations values and compute the local velocity
+        # 4. Guo-shift the velocity
+        # 5. Collide with Guo's forcing on local populations
+        # 6. Stream in 'push' mode
+        
+        _swap_kernel_idpy = """"""
+        _swap_kernel_idpy += _codify_comment("Getting Cartesian Coordinates")
+        _swap_kernel_idpy += \
+            _get_cartesian_coordinates_macro(
+                declared_variables = self.declared_variables,
+                declared_constants = self.declared_constants,
+                _root_coordinate = root_coord,
+                _dim_sizes = self.dim_sizes_macros,
+                _dim_strides = self.dim_strides_macros,
+                _type = 'SType', declare_const_flag = True,
+                _lexicographic_index = 'g_tid'
+            ) + \
+            _codify_newl
+        
+        _swap_kernel_idpy += _codify_comment("Computing the Shan-Chen Force")        
+        _swap_kernel_idpy += \
+            self.idpy_sc_force.ForceCodeMultiPhase(
+                declared_variables = self.declared_variables,
+                declared_constants = self.declared_constants,
+                pressure_mode = pressure_mode, psi_array = 'psi', 
+                use_ptrs = use_ptrs, 
+                root_dim_sizes = root_dim_sizes, 
+                root_strides = root_strides,
+                root_coord = root_coord, 
+                lex_index = 'g_tid'
+            ) + \
+            _codify_newl
+        
+        _swap_kernel_idpy += _codify_comment("Loading the local value of the density")
+        _swap_kernel_idpy += \
+            _codify_declaration_const_check(
+                'ln_0', _array_value('n', 'g_tid', use_ptrs), 'NType', 
+                declared_variables = self.declared_variables, 
+                declared_constants = self.declared_constants, 
+                declare_const_flag = True
+            ) + \
+            _codify_newl
+        
+        _swap_kernel_idpy += \
+            _codify_comment("Loading the local value of the populations and keep it")
+        _swap_kernel_idpy += \
+            _codify_comment("Computing the local value of the velocity")        
+        _swap_kernel_idpy += \
+            self.idpy_stencil.VelocityCode(
+                declared_variables = self.declared_variables, 
+                declared_constants = self.declared_constants, 
+                arrays = ['pop'], arrays_types = ['PopType'],
+                n_type = 'NType', u_type = 'UType',
+                root_n = 'ln', root_u = 'lu', 
+                lex_index = 'g_tid', keep_read = True,
+                ordering_lambdas = [ordering_lambda_pop], 
+                use_ptrs = use_ptrs, 
+                declare_const_dict = {'arrays_xi': True, 'moments': False}
+            )
+        
+        _swap_kernel_idpy += _codify_comment("Guo's Velocity shift")
+        _swap_kernel_idpy += \
+            self.idpy_guo_forcing.ShiftVelocityCode(
+                n_sets = ['ln_0'], u_sets = ['lu_0'], f_sets = ['d_psi']
+            )
+        _swap_kernel_idpy += _codify_newl
+               
+        '''
+        Preparing the variables for collision plus streaming according to the pressure mode
+        '''
+        _swap_tuples_eq_prod, _swap_tuples_guo_prod = [], []        
+
+        _swap_code, _swap_tuples_eq_prod = \
+            self.idpy_equilibria.SetMomentsProductsCode(
+                declared_variables = self.declared_variables,
+                declared_constants = self.declared_constants,
+                order = 2, root_n = 'ln_0', root_u = 'lu_0', 
+                mom_type = 'PopType', pressure_mode = pressure_mode
+            )
+        _swap_kernel_idpy += _swap_code + _codify_newl
+
+        _swap_code, _swap_tuples_guo_prod = \
+            self.idpy_guo_forcing.SetMomentsProductsCode(
+                declared_variables = self.declared_variables, 
+                declared_constants = self.declared_constants, 
+                order = 2, root_f = 'd_psi', root_u = 'lu_0',
+                mom_type = 'PopType', pressure_mode = pressure_mode
+            )
+        _swap_kernel_idpy += _swap_code + _codify_newl
+        
+        _swap_kernel_idpy += _codify_comment("Computing equilibrium, collision and stream")
+        _swap_kernel_idpy += \
+            self.idpy_collision.SRTCollisionPlusGuoCode(
+                declared_variables = self.declared_variables,
+                declared_constants = self.declared_constants, 
+                ordering_lambda = ordering_lambda_pop,
+                dst_arrays_var = 'pop',
+                stencil_obj = self.idpy_stencil,
+                eq_obj = self.idpy_equilibria, 
+                guo_obj = self.idpy_guo_forcing,
+                neq_pop = 'lpop', pressure_mode = pressure_mode,
+                tuples_eq = _swap_tuples_eq_prod, 
+                tuples_guo = _swap_tuples_guo_prod,
+                pos_type = 'SType', use_ptrs = use_ptrs, collect_mul = collect_mul, 
+                root_dim_sizes = root_dim_sizes, root_strides = root_strides, 
+                root_coord = root_coord, lex_index = 'g_tid',
+                declare_const_dict = {'cartesian_coord_neigh': True}
+            )
+
+        
+        self.kernels[IDPY_T] = """
+        if(g_tid < V){
+        """ + \
+        _swap_kernel_idpy + \
+        """
+        }
+        """
+        
 class K_ForceCollideStreamWallsSCMPMeta(IdpyKernel):
     def __init__(self, custom_types = {}, constants = {}, f_classes = [],
                  optimizer_flag = None, 
